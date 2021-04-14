cf9fb3f12431326469ce183e7635d0dff4a66fb6
Himanshu121212 <62050010+Himanshu121212@users.noreply.github.com>
2021-04-14 03:51:42 -0400

    Add files via upload

---
+++ Password.java
@@ -0,0 +1,47 @@
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.*;
+import java.nio.file.*;
+import java.security.MessageDigest;
+import javax.swing.*;  
+
+//A helper file for comparing password entered with hashed password stored in secure file.
+class PasswordTools {
+
+    //Hash the password and compare it to the hash stored in the file.
+    public static boolean validPassword(String password, Path path) {
+ 
+        byte byteData[] = null;
+        byte toCompare[] = null;
+        try {
+            MessageDigest md = MessageDigest.getInstance("SHA-256");
+            md.update(password.getBytes());
+            byteData = md.digest();
+            toCompare = Files.readAllBytes(path);
+        } catch (Exception e) {
+            System.out.println("Failed to read file");
+        }
+        if (Arrays.equals(toCompare, byteData)) {
+            return true;
+        }
+        return false;
+    }
+
+    //Prompts the user to enter their password and then verifies it.
+    static void verifyPassword(Path path) {
+        
+ 	   JFrame f=new JFrame();  
+        String password=JOptionPane.showInputDialog(f,"Enter Password");    
+        System.out.println("What is your password?");
+        while (true) {
+            if (validPassword(password, path)) {
+                System.out.println("Authenticated!");
+                break;
+            } else {
+                System.out.println("Incorrect password, try again.");
+            }
+        }
+    }
+}
